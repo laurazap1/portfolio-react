ddd799e4a64c1ba69b50799ba7440740926a1199
laurazap1 <zapatalcollege@gmail.com>
2025-08-28 21:08:12 -0400

    React init and added  components to app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Components/NavBar/Navbar";
+import Hero from "./Components/Hero/Hero";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
