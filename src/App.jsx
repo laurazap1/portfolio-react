366aeda468072259a785abd047c6fbd69a47f190
laurazap1 <zapatalcollege@gmail.com>
2025-09-12 22:58:23 -0500

    Add About component and update global styles for improved layout and aesthetics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Components/NavBar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 };
